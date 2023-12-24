d1b641082939ac4c79121f60db04d9d2b132993a
hb21a034 <119714295+hb21a034@users.noreply.github.com>
2023-12-24 23:46:42 +0900

    Fix rank system & ui.
    ランキングをステージごとに分離した
    setting画面のUIを追加した

--- Assets/Main/Script/System/RankSystem.cs
+++ Assets/Main/Script/System/RankSystem.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 
 public class RankSystem : MonoBehaviour
 {
+    [SerializeField] int stage = 0;
+    [SerializeField] static int stageNum = 0;
+    int maxStage = 3;
+
+    void Start()
+    {
+        stageNum = stage * 100;
+    }
+
     public static int[] LoadRanking()
     {
         int[] Ranking = new int[6];
 
         for (int i = 1; i < 6; i++)
         {
-            Ranking[i - 1] = PlayerPrefs.GetInt(i.ToString());
+            int rankName = i + stageNum;
+            Ranking[i - 1] = PlayerPrefs.GetInt((rankName).ToString());
         }
         return Ranking;
     }
@@ -23,7 +34,20 @@ public class RankSystem : MonoBehaviour
         System.Array.Sort(nowRanking);
         for (int i = 1; i < 6; i++)
         {
-            PlayerPrefs.SetInt(i.ToString(), nowRanking[^i]);
+            int rankName = i + stageNum;
+            PlayerPrefs.SetInt(rankName.ToString(), nowRanking[^i]);
+        }
+    }
+
+    public void ResetRanking()
+    {
+        for (int i = 1; i < 6; i++)
+        {
+            for (int j = 1; j < maxStage; j++)
+            {
+                int rankName = j + stageNum;
+                PlayerPrefs.SetInt(rankName.ToString(), 0);
+            }
         }
     }
 }
